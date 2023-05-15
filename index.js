3aaab3942e28aa7cefd5e2fb97c92053bd735e61
codr <codr619@gmail.com>
2023-05-15 11:46:39 +0530

    first

---
+++ index.js
@@ -0,0 +1,25 @@
+gameStart=confirm("Do you want to play a game?")
+if(gameStart){
+rockPaper=confirm("Do you want to play rock paper scissirs?")
+if(rockPaper){
+    choice=prompt("choose rock paper or scissors...")
+    choice=choice.trim()
+    
+    if(choice==="rock"||choice==="paper"||choice==="scissors")
+    {
+let opt=["rock","paper","scissors"]
+comp=opt[Math.floor(Math.random()*3)]
+
+}
+    }
+    else {
+        alert(`only enter "rock" or "paper" or "scissors"`)
+    }
+}
+else{
+    alert("i guess not ineterested")
+}
+}
+else{
+    alert("another tijme then")
+}
